1dbfb15d3a2bcab2df3382b8e94a4d55c6c21939
fadimata tillman <fadi.tillman@gmail.com>
2020-03-10 05:54:22 +0000

    start project code

--- lib/fadiprojectcli/cli.rb
+++ lib/fadiprojectcli/cli.rb
@@ -1,7 +1,22 @@
 class Fadiprojectcli::Cli
   
-  def start
+  
+    #start should greet user and give a description (loop to keep asking for info)
+    #get data from API file
+    
+    #create new costume object
+    #all inside start method 
+    #  work with input 
+    #expect input 
+    # depending input do something 
+    #exit command 
+    #if input == exit
+    #kill program , end message .
+   
+    def start
     puts "Hello!/ Bonjour!"
+    puts "geting data from api ...please wait"
+    puts "new object"
     #binding.pry
   end 
   
