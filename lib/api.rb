911fd22696faaff6802208cc781fc95263d4d3d5
fadimata tillman <fadi.tillman@gmail.com>
2020-03-10 06:16:47 +0000

    code skull

---
+++ lib/api.rb
@@ -0,0 +1,5 @@
+#find api 
+#use set url 
+#build hashes out for object 
+#call custome class 
+#send those back to our cli
--- lib/fadiprojectcli/cli.rb
+++ lib/fadiprojectcli/cli.rb
@@ -3,22 +3,31 @@ class Fadiprojectcli::Cli
   
     #start should greet user and give a description (loop to keep asking for info)
     #get data from API file
-    
     #create new costume object
     #all inside start method 
-    #  work with input 
-    #expect input 
-    # depending input do something 
-    #exit command 
-    #if input == exit
-    #kill program , end message .
-   
+    
     def start
     puts "Hello!/ Bonjour!"
     puts "geting data from api ...please wait"
     puts "new object"
     #binding.pry
   end 
-  
+  #  work with input 
+    #expect input 
+    # depending input do something 
+    #exit command 
+    #if input == exit
+    #kill program , end message .
+   
+   def display_infos
+     puts "please make a selection"
+      input = "gets.strip.downcase"
+      if input == adresse
+        puts "buyer agent  list "
+         else 
+           puts "write a correct adresse"
+           puts "invalide adresse"
+           
+   end 
 end 
   
