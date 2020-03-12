1635806665075ca09ea7c524de157252664568ec
fadimata tillman <fadi.tillman@gmail.com>
2020-03-12 05:41:27 +0000

    api

--- lib/api.rb
+++ lib/api.rb
@@ -6,6 +6,7 @@
 
 class Fadiprojectcli::Api
   def self.get_schools
-    @schools_hash = HTTparty.get("")
+    @schools_hash = HTTparty.get("https://api.yelp.com/v3/businesses/searchsPyWsvqJfKaC2ttOpmyQectb4xMHBmpDxf7L4GFXq1ojPTPrNu9_BajtIPr_2J5CpJmkL8ZyZhc_X_L0u1Fwe1-r19gTWS0eWF3AFn4V-iAJHaVbzP83dZ0smb9pXnYx")
+    binding.pry
 end 
 end
