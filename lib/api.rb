d8bb4f80dc5073d254829f8571e13eba93fa4faf
fadimata tillman <fadi.tillman@gmail.com>
2020-03-13 15:17:56 +0000

    yelp link api

--- lib/api.rb
+++ lib/api.rb
@@ -6,7 +6,7 @@
 
 class Fadiprojectcli::Api
   def self.get_businesses
-    @businesses_hash = HTTparty.get("https://api.yelp.com/v3/businesses)
+    @businesses_hash = HTTparty.get("https://api.yelp.com/v3/businesses/search?location=Las Vegas")
     binding.pry
 end 
 end
