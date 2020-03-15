a9637da52db952b33fb173d22f2d17b582283d65
fadimata tillman <fadi.tillman@gmail.com>
2020-03-15 00:37:17 +0000

    code

--- lib/fadiprojectcli/api.rb
+++ lib/fadiprojectcli/api.rb
@@ -9,12 +9,12 @@ class Fadiprojectcli::API
     @charactere_hash = HTTParty.get("https://swapi.co/api/people/1/")
     binding.pry
     stuff_obj = {
-      name: @charactere_hash_hash.["name"]
-      height: @charactere_hash_hash.["height"]
-      gender: @charactere_hash.["gender"]
-      eye_color: @charactere_hash.["eye_color"]
-      species: @charactere_hash.["species"]
-      homeworld: @charactere_hash.["homeworld"]
+      name: @charactere_hash_hash["name"],
+      height: @charactere_hash_hash["height"],
+      gender: @charactere_hash["gender"],
+      eye_color: @charactere_hash["eye_color"],
+      # species: @charactere_hash["https://swapi.co/api/species/1/"],
+      # homeworld: @charactere_hash["https://swapi.co/api/planets/1/"]
       
       
     }
