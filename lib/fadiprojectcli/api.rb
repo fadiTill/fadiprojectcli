ec96fe8c204e8065f0a6300c0f8353722d88db3d
fadimata tillman <fadi.tillman@gmail.com>
2020-03-16 02:30:33 +0000

    code

--- lib/fadiprojectcli/api.rb
+++ lib/fadiprojectcli/api.rb
@@ -4,17 +4,24 @@
 #call custome class 
 #send those back to our cli
 
+
+
 class Fadiprojectcli::API
+  
+
+
+
+  
   def self.get_stuff
-    @charactere_hash = HTTParty.get("https://swapi.co/api/people/1/")
-    binding.pry
-    characteres_obj = {
-      name: @charactere_hash["name"],
-      height: @charactere_hash["height"],
-      skin_color: @charactere_hash["skin_color"],
-      eye_color: @charactere_hash["eye_color"],
-      gender: @charactere_hash["gender"]
-       }
+    @charactere_hash = HTTParty.get("http://swapi.co/api/people/1/")
+    @infos_obj = {
+         name: @charactere_hash["name"],
+        height: @charactere_hash["height"],
+        eye_color: @charactere_hash["eye_color"],
+        gender: @charactere_hash["gender"] }
+      binding.pry
+  
+      
 #     Fadiprojectcli::Information.new(characteres_obj)
  end  
 end
