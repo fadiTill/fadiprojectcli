b8f14e611ed6670633ae90e91ba0a49ce096c0c7
fadimata tillman <fadi.tillman@gmail.com>
2020-03-16 23:12:31 +0000

    code

--- lib/fadiprojectcli/cli.rb
+++ lib/fadiprojectcli/cli.rb
@@ -9,9 +9,9 @@
     def start
       puts "Welcome to star wars fan search!"
       puts ".................................................................................................................."
-      puts "this website will help you found start wars  charactere information.
+      puts "this website will help you found start wars  charactere information."
       puts "You can make your seach of characteres by choosing a  random number between 1 to 87"
-      puts"....................................................................................................................'"
+      puts "....................................................................................................................'"
       puts "please wait a moment while we search your result"
       puts "...................................................................................................................."
       puts "please make an index selection ( index number 2 for height, 3 for eye_color and,4 for gender.)"
@@ -38,17 +38,17 @@ def show_results
     input=gets.strip
     @information = @objects [input.to_i - 1]
     if (@information)
+      puts "here is the height"
+      show_results
       puts"#{@information.height}"
-       puts "here is the height"
-      show_results
     elsif @information = @objects [input.to_i - 2]
-    puts "here is the eye_color"
-    puts"#{@information.eye_color}"
-      show_results
+       puts "here is the eye_color"
+       show_results
+       puts"#{@information.eye_color}"
     elsif @information = @objects [input.to_i - 3]
-    puts"#{@information.gender}"
-    puts "here is the gender"
-      show_results
+       puts "here is the gender"
+       puts"#{@information.gender}"
+       show_results
     elsif input != "#{@information.height}" || input!= "#{@information.eye_color}" || input !="#{@information.gender}" || input == nil
        puts"Please, write a correct search criteria"
      elsif input == quit
