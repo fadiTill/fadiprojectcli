032e286d28e12ebfcf98ff69ea6d68c7b091cb5f
fadimata tillman <fadi.tillman@gmail.com>
2020-03-17 05:19:51 +0000

    code

--- lib/fadiprojectcli/cli.rb
+++ lib/fadiprojectcli/cli.rb
@@ -28,7 +28,7 @@ def show_results
     
   
 
-      while input > 0 
+       
        puts "please make a selection by index number"
       input = gets.strip
       @information = @objects[input.to_i - 2]
@@ -49,64 +49,17 @@ def show_results
     end
    end
       
-    #   elsif input != "#{@information.height}" || input!= "#{@information.eye_color}" || input !="#{@information.gender}" || input == nil
-    # # puts"Please, write a correct search criteria"
-       
+    
        
     def quit 
       puts "thank you for visiting our website, goodbye!" 
      end
    end
- end
-     
-       
-       
-       
-       
-       
-        # puts
-#       elsif(input=="quit")
-#       quit
-#     else
-#       puts "write a correct search criteria"
-#       show_results
-#   end
-# end
  
-   
-  
      
-     
-  #   #puts #{obj.name} - {obj.adresse}
-  # end
-  #   input = gets.strip.downcase
-     
-     
-  #   if input == "people" #1
-  #     puts"...........  infos......... "
-  #     puts" infos people"
-  #     #if obj = @object[0]
-  #     #puts obj.name
-  #     #show_starinfos_results
-  #   elsif input ==  "planet"
-  #     puts"...........planet.................."
-  #     puts"Planet infos"
-  #     show_starinfos_results
-  #   elsif input == "species"
-  #     puts"............species infos.............."
-  #     puts"SPECIES INFOS"
-  #     show_starinfos_results
-  #   elsif input != "people" || input!="Planet" || input !="species" || input == nil
-  #   puts"write a correct search criteria"
-  #   end
-  
-     
-  #   def leave_search
-  #     input == "leave search"
-  #     puts"thank you for visiting our website,goodbye!" 
-  #   end 
-  # end 
-  
-
-  
-  
+       
+       
+       
+       
+       
+ 
